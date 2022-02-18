b254f52ea06cc046eb238137cb3c1857512368f3
Yannik Brändle <yannik.braendle@th-bingen.de>
2022-02-18 17:50:12 +0100

    added convenience function

--- AnalyserDefinitions.h
+++ AnalyserDefinitions.h
@@ -1,15 +1,15 @@
 
-//basic Parameters
+// basic Parameters
 #define OCTAVES 9
 #define NOTES 12
 
 #define VALUE_CUTOFF 15
 
-//cutoff at B3 since the Noisefloor makes it difficult to correctly identify the peaks up to ~250Hz
+// cutoff at B3 since the Noisefloor makes it difficult to correctly identify the peaks up to ~250Hz
 #define GRAPHING_MIN_FREQ 246
 #define GRAPHING_MAX_FREQ 4000
 
-//peak definitions
+// peak definitions
 #define PEAK_DIFFERENCE_PER_STEP 2
 #define PEAK_DIFFERENCE_OFFSET 5
 #define PEAK_DISTANCE 30
@@ -17,7 +17,7 @@
 #define PEAK_SEARCH_DISTANCE_INCREASE 10
 #define PEAK_SEARCH_INCREASE_STEPSIZE 500
 
-//recording Parameters
+// recording Parameters
 #define SAMPLE_RATE 44100
 #define FRAMES_PER_BUFFER 512
 #define NUM_SECONDS 0.5
@@ -25,7 +25,8 @@
 
 constexpr int totalFrames = NUM_SECONDS * SAMPLE_RATE;
 constexpr int numSamples = totalFrames * NUM_CHANNELS;
-constexpr int resultSize = totalFrames;
+constexpr int resultSize = (GRAPHING_MAX_FREQ - GRAPHING_MIN_FREQ) * NUM_SECONDS;
+constexpr double calcHz(int x) { return (x / NUM_SECONDS) + GRAPHING_MIN_FREQ; }
 
 #define PA_SAMPLE_TYPE paFloat32
 typedef float SAMPLE;
