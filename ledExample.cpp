8cef8d1b598389b30782e78746a7b3cb878043ce
Yannik Brändle <yannik.braendle@th-bingen.de>
2022-02-23 17:10:36 +0100

    further experiments with LEDs

--- ledExample.cpp
+++ ledExample.cpp
@@ -4,6 +4,8 @@
 
 #include <ws2811/ws2811.h>
 
+#include <stdlib.h>
+
 int width = 100;
 
 #define TARGET_FREQ WS2811_TARGET_FREQ
@@ -33,19 +35,28 @@ ws2811_t ledstrip =
             },
 };
 
+//blue green red
 ws2811_led_t dotcolors[] =
     {
-        0x00200000, // red
-        0x00201000, // orange
-        0x00202000, // yellow
-        0x00002000, // green
-        0x00002020, // lightblue
-        0x00000020, // blue
-        0x00100010, // purple
-        0x00200010, // pink
+        0x00110000, // blue (higher numbers == brighter colours)
+        0x00001100, // green
+        0x00000011, // red
+        0x00002000, // 
+        0x00002020, // 
+        0x00000020, // 
+        0x00100010, // 
+        0x00200010, // 
 };
+void clearLEDS()
+{
+    // clear LEDs
+    for (int i = 0; i < 96; i++)
+    {
+        ledstrip.channel[0].leds[i] = 0;
+    }
+}
 
-int main()
+int main(int argc, char **argv)
 {
     ws2811_return_t ret;
     if ((ret = ws2811_init(&ledstrip)) != WS2811_SUCCESS)
@@ -53,27 +64,28 @@ int main()
         fprintf(stderr, "ws2811_init failed: %s\n", ws2811_get_return_t_str(ret));
         return ret;
     }
-    for (int loop = 0; loop < 15; loop++)
-    {
-
-        for (int i = 0; i < 96; i++)
-        {
-
-            ledstrip.channel[0].leds[i] = dotcolors[loop%8];
-            ws2811_render(&ledstrip);
-            usleep(10000);
-            ledstrip.channel[0].leds[i] = 0;
-            ws2811_render(&ledstrip);
-        }
-    }
-    // for (int i = 0; i < 12; i++)
+    // for (int loop = 0; loop < 15; loop++)
     // {
 
-    //     ledstrip.channel[0].leds[8] = dotcolors[i];
-    //     ws2811_render(&ledstrip);
-    //     usleep(1000000);
-    //     ledstrip.channel[0].leds[8] = 0;
-    //     ws2811_render(&ledstrip);
+    //     for (int i = 0; i < 96; i++)
+    //     {
+
+    //         ledstrip.channel[0].leds[i] = dotcolors[loop%8];
+    //         ws2811_render(&ledstrip);
+    //         usleep(10000);
+    //         ledstrip.channel[0].leds[i] = 0;
+    //         ws2811_render(&ledstrip);
+    //     }
     // }
+    char *p;
+    int col = strtol(argv[1], &p, 16);
+    printf("%s -> %d\n", argv[1], col);
+    for (int i = 0; i < 96; i++)
+    {
+        ledstrip.channel[0].leds[i] = col;
+    }
+    ws2811_render(&ledstrip);
+    // clearLEDS();
+    // ws2811_render(&ledstrip);
     return 0;
 }
